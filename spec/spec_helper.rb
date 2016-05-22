0ada1025cc04ec10a8d0ce38332146bcd06f8685
sharonrussell <sharon.russell@comparethemarket.com>
2016-05-22 09:41:23 +0100

    forgot the spec_helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,14 @@
+require_relative '../swapi_getter'
+require_relative '../opening_crawl'
+require_relative '../crawl_swapper'
+
+RSpec.configure do |config|
+  # Use color in STDOUT
+  config.color = true
+
+  # Use color not only in STDOUT but also in pagers and files
+  config.tty = true
+
+  # Use the specified formatter
+  config.formatter = :documentation # :progress, :html, :textmate
+end
